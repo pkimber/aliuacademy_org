de2505271cb939587a54e605706745223a41fafc
Patrick Kimber <code@pkimber.net>
2013-11-13 18:47:44 +0100

    version 0.0.04

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
             'templates/aliu/*.*',
         ],
     },
-    version='0.0.03',
+    version='0.0.04',
     description='Aliu Academy web site',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
