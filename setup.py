c25c172358e121bd72bef28c80554d6ae8fa2e66
Patrick Kimber <code@pkimber.net>
2015-02-02 13:35:57 +0100

    version 0.0.18

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
             'templates/web/*.*',
         ],
     },
-    version='0.0.17',
+    version='0.0.18',
     description='Aliu Academy web site',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
