f53d2fbd7555892c1bb039e3715de9f8c1532c44
Patrick Kimber <code@pkimber.net>
2015-03-19 19:09:55 +0000

    update from 'kalite' - is working #203

---
+++ academy/local_settings.py
@@ -0,0 +1,6 @@
+# -*- encoding: utf-8 -*-
+
+# PJK 11/03/2015 Can't find these in the documentation, but we seem to need
+# them.  Perhaps they are to do with Django 1.7?
+PRODUCTION_PORT = 8080
+KALITE_TEST_RUNNER = 'django.test.runner.DiscoverRunner'
