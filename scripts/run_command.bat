54b63c520146ae76e2216ad404855eed1ad0be7d
Patrick Kimber <code@pkimber.net>
2015-03-25 09:37:05 +0000

    rename 'kalite' to 'academy' #203

--- scripts/run_command.bat
+++ scripts/run_command.bat
@@ -2,11 +2,11 @@
 
 rem determine the script directory (could be in scripts, could be in root folder)
 set "SCRIPT_DIR=%~dp0"
-if exist "%SCRIPT_DIR%\kalite" (
-    set "KALITE_DIR=%SCRIPT_DIR%\kalite"
+if exist "%SCRIPT_DIR%\academy" (
+    set "KALITE_DIR=%SCRIPT_DIR%\academy"
 ) else (
     set "SCRIPT_DIR=%SCRIPT_DIR%\scripts"
-    set "KALITE_DIR=%SCRIPT_DIR%\..\kalite"
+    set "KALITE_DIR=%SCRIPT_DIR%\..\academy"
 )
 
 rem This file will run an arbitrary management command, within the confines of our "sandbox" script
